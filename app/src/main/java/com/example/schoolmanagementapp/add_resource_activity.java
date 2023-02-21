e5321e2280d3d62399f10cc68b159899d01eb87f
dmasamba <dmasamba.dmn@gmail.com>
2023-02-21 14:24:30 -0600

    first commit

---
+++ app/src/main/java/com/example/schoolmanagementapp/add_resource_activity.java
@@ -0,0 +1,14 @@
+package com.example.schoolmanagementapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class add_resource_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_resource);
+    }
+}
